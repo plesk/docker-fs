9b138f230f965fd6d90abd0458cc41d8bf852bba
Aleksandr Voronkov <avoronkov@plesk.com>
2020-02-22 13:35:15 +0700

    TECH add test docker image

---
+++ test/image/logger.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+var (
+	logdir      string
+	contentFile string
+)
+
+func init() {
+	flag.StringVar(&logdir, "logdir", "/tmp", "log dir")
+	flag.StringVar(&contentFile, "content-file", "content.txt", "content file")
+}
+
+func main() {
+	flag.Parse()
+	if err := os.MkdirAll(logdir, 0755); err != nil {
+		log.Fatal(err)
+	}
+	for {
+		now := time.Now()
+		name := now.Format("15-04")
+		logfile := filepath.Join(logdir, fmt.Sprintf("log.%v.txt", name))
+		content := readContent(contentFile)
+		func() {
+			f, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				log.Print(err)
+				return
+			}
+			defer f.Close()
+			fmt.Printf("%v : %v\n", now, content)
+			fmt.Fprintf(f, "%v : %v\n", now, content)
+		}()
+		time.Sleep(7 * time.Second)
+	}
+}
+
+func readContent(path string) string {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Printf("Error: %v", err)
+		return ""
+	}
+	return strings.TrimSpace(string(content))
+}
